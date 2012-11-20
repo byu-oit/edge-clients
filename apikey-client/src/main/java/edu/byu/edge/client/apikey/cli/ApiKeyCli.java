967d393bd786aba5e8d0a93000fa7d15f793567c
Wyatt Taylor <wct5@byu.edu>
2012-11-20 11:25:03 -0700

    release versions

--- apikey-client/src/main/java/edu/byu/edge/client/apikey/cli/ApiKeyCli.java
+++ apikey-client/src/main/java/edu/byu/edge/client/apikey/cli/ApiKeyCli.java
@@ -212,7 +212,7 @@ public final class ApiKeyCli {
 				OUT.format("Nonce Key:   %s\n", nonce.getNonceKey());
 				OUT.format("Nonce Value: %s\n", nonce.getNonceValue());
 				OUT.format("HMAC:        %s\n", hmac);
-				OUT.format("Auth Header: %s\n", head);
+				OUT.format("Auth Header: %s\n", head.replace("Nonce-Encoded-WsSession-Key", "Nonce-Encoded-API-Key"));
 			} catch (final Throwable t) {
 				OUT.println("Error getting nonce:");
 				t.printStackTrace(OUT);
