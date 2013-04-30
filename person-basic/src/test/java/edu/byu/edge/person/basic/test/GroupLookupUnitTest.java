70f853e81a7a9120a6bc8841cdb4084205fe2f4e
thirschi <timothy_hirschi@byu.edu>
2013-04-30 10:15:13 -0600

    Wrote test case for groupLookup

--- person-basic/src/test/java/edu/byu/edge/person/basic/test/GroupLookupUnitTest.java
+++ person-basic/src/test/java/edu/byu/edge/person/basic/test/GroupLookupUnitTest.java
@@ -2,11 +2,17 @@ package edu.byu.edge.person.basic.test;
 
 import edu.byu.edge.person.basic.GroupLookup;
 import org.apache.log4j.Logger;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by IntelliJ IDEA.
@@ -26,4 +32,16 @@ public class GroupLookupUnitTest {
 	@Autowired
 	@Qualifier("groupLookup")
 	private GroupLookup groupLookup;
+
+	@Test
+	public void testGroupLookup() throws Exception {
+		List<String> groups = groupLookup.getGroupsForPerson("249262982");
+		assertTrue(groups.size() > 0);
+		groups = groupLookup.getAllGroupIds();
+		assertTrue(groups.size() > 0);
+		boolean group =groupLookup.isMember("249262982", "WHATEVERGROUP");
+		assertFalse(group);
+		group = groupLookup.isMember("249262982", "SDFA");
+		assertTrue(group);
+	}
 }
