e64fd04ac6c2089fa05b3624f3218b67710decf2
Wyatt Taylor <wct5@byu.edu>
2014-07-16 16:14:04 -0600

    chartBlock - added some logging for test case, changed line endings to be more consistent

--- chartBlock/src/test/java/edu/byu/chartblock/test/AccountCodeValidationImplTest.java
+++ chartBlock/src/test/java/edu/byu/chartblock/test/AccountCodeValidationImplTest.java
@@ -1,6 +1,7 @@
 package edu.byu.chartblock.test;
 
 import edu.byu.chartblock.AccountCodeValidation;
+import edu.byu.chartblock.ValidateChartBlockResult;
 import org.apache.log4j.Logger;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,7 +9,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 /**
  * Created by IntelliJ IDEA.
  * User: thirschi
@@ -29,7 +37,14 @@ public class AccountCodeValidationImplTest extends BaseTest {
 	@Test
 	public void testAccountCodeValidation() throws Exception {
 		assertNotNull("need client", accountCodeValidation);
-		accountCodeValidation.getAccount("11360000-6010-00000");
+		final ValidateChartBlockResult account = accountCodeValidation.getAccount("11360000-6010-00000");
+		assertTrue(account.isSuccessful());
+		final Map<String, String> map = account.getFields();
+		final List<String> keys = new ArrayList<String>(map.keySet());
+		Collections.sort(keys);
+		for (final String k : keys) {
+			LOG.debug(k + " -> " + map.get(k));
+		}
 		LOG.debug("Done.");
 	}
 }
