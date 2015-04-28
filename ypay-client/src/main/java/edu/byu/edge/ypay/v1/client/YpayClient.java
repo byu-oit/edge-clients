33af8b3574dfdd514bfd7d2fc663015f255c5e97
Eric Romrell <eric_romrell@byu.edu>
2015-04-28 13:58:08 -0600

    Added new functions

--- ypay-client/src/main/java/edu/byu/edge/ypay/v1/client/YpayClient.java
+++ ypay-client/src/main/java/edu/byu/edge/ypay/v1/client/YpayClient.java
@@ -1,12 +1,41 @@
 package edu.byu.edge.ypay.v1.client;
 
 import edu.byu.edge.ypay.v1.domain.invoice.InvoiceListType;
+import edu.byu.edge.ypay.v1.domain.invoice.LineItemType;
 
 import java.util.Date;
+import java.util.List;
 
 /**
  * Created by wct5 on 2/18/15.
  */
 public interface YpayClient {
 	public InvoiceListType findInvoicesForPersonOnDay(String personId, Date day);
+
+	/**
+	 * Creates an invoice using empty returnUrl and notificationUrl
+	 * @param clientTransactionId Unique ID used in table
+	 * @param owner Foreign key to the person table
+	 * @param lineItemList Must contain at least one LineItemType
+	 * @return Invoice ID that was created
+	 */
+	public String createInvoice(String clientTransactionId, String owner, List<LineItemType> lineItemList);
+
+	/**
+	 * Creates an invoice
+	 * @param clientTransactionId Unique ID used in table
+	 * @param returnUrl
+	 * @param notificationUrl
+	 * @param owner Foreign key to the person table
+	 * @param lineItemList Must contain at least one LineItemType
+	 * @return Invoice ID that was created
+	 */
+	public String createInvoice(String clientTransactionId, String returnUrl, String notificationUrl, String owner, List<LineItemType> lineItemList);
+
+	/**
+	 * Marks an invoice as cancelled
+	 * @param invoiceId Unique ID provided when the invoice was created
+	 * @return Whether or not the cancellation happened properly
+	 */
+	boolean deleteInvoice(String invoiceId);
 }
--- ypay-client/src/main/java/edu/byu/edge/ypay/v1/client/YpayClientImpl.java
+++ ypay-client/src/main/java/edu/byu/edge/ypay/v1/client/YpayClientImpl.java
@@ -1,17 +1,19 @@
 package edu.byu.edge.ypay.v1.client;
 
 import edu.byu.auth.client.CredentialClient;
-import edu.byu.edge.ypay.v1.domain.invoice.InvoiceListType;
+import edu.byu.edge.ypay.v1.domain.invoice.*;
+import org.apache.commons.io.IOUtils;
 
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBElement;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Unmarshaller;
+import javax.xml.bind.*;
 import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by wct5 on 2/18/15.
@@ -19,12 +21,15 @@ import java.util.Date;
 public class YpayClientImpl implements YpayClient {
 
 	private static final String SEARCH_STRING = "%sinvoices/search?clientSystemId=%s&paidByIds=%s&start=0&results=64&paymentStartDate=%s&paymentEndDate=%s";
+	private static final String CREATE_INVOICE_STRING = "%s%s/invoices";
+	private static final String DELETE_INVOICE_STRING = CREATE_INVOICE_STRING + "/%s";
 
 	protected final String baseUrl;
 	protected final String clientSystemId;
 	protected final CredentialClient credentialClient;
 	protected JAXBContext invoiceContext;
 	protected ThreadLocal<Unmarshaller> unmarshallerThreadLocal;
+	protected ThreadLocal<Marshaller> marshallerThreadLocal;
 
 	public YpayClientImpl(final String clientSystemId, final CredentialClient credentialClient) {
 		this("https://ypay.byu.edu/payments/service/rest/v1/", clientSystemId, credentialClient);
@@ -42,9 +47,9 @@ public class YpayClientImpl implements YpayClient {
 
 	public void initialize() throws Exception {
 		unmarshallerThreadLocal = new InvoiceUnmarshallerThreadLocal(invoiceContext);
+		marshallerThreadLocal = new InvoiceMarshallerThreadLocal(invoiceContext);
 	}
 
-	@Override
 	public InvoiceListType findInvoicesForPersonOnDay(final String personId, final Date day) {
 		PrintStream urlout = null;
 		InputStream urlin = null;
@@ -89,6 +94,102 @@ public class YpayClientImpl implements YpayClient {
 		}
 	}
 
+	@Override
+	public String createInvoice(String clientTransactionId, String owner, List<LineItemType> lineItemList) {
+		return createInvoice(clientTransactionId, "", "", owner, lineItemList);
+	}
+
+	@Override
+	public String createInvoice(String clientTransactionId, String returnUrl, String notificationUrl, String owner, List<LineItemType> lineItemList) {
+		//Check for valid values
+		if(clientTransactionId == null || clientTransactionId.isEmpty()) {
+			throw new IllegalArgumentException("Invalid client transaction ID");
+		}
+		if(owner == null || owner.isEmpty()) {
+			throw new IllegalArgumentException("Invalid owner");
+		}
+		if(lineItemList == null || lineItemList.isEmpty()) {
+			throw new IllegalArgumentException("Invalid line items");
+		}
+
+		//Populate the invoice request body
+		InvoiceRequestType invoiceRequestType = new InvoiceRequestType();
+		invoiceRequestType.setClientSystemTransactionId(clientTransactionId);
+		invoiceRequestType.setReturnUrl(returnUrl);
+		invoiceRequestType.setNotificationUrl(notificationUrl);
+		invoiceRequestType.setOwner(owner);
+		invoiceRequestType.setPaymentSourceAllowed(PaymentSourceAllowedType.ALL);
+
+		LineItemListType lineItems = new LineItemListType();
+		lineItems.getLineItem().addAll(lineItemList);
+		invoiceRequestType.setLineItems(lineItems);
+
+		try {
+			final URL url = new URL(String.format(CREATE_INVOICE_STRING, baseUrl, clientSystemId));
+			final URLConnection connection = url.openConnection();
+			connection.setRequestProperty("Accept", "application/xml,text/xml");
+			connection.setRequestProperty("Authorization", credentialClient.obtainAuthorizationHeaderString());
+			connection.setRequestProperty("Content-Type", "application/xml");
+			connection.setDoOutput(true);
+
+			marshallerThreadLocal.get().marshal(invoiceRequestType, connection.getOutputStream());
+
+			final Map<String, List<String>> headerFields = connection.getHeaderFields();
+			if(!headerFields.containsKey("Location")) {
+				throw new IOException("Response missing 'Location' header");
+			}
+			List<String> locations = headerFields.get("Location");
+			if(locations.size() != 1) {
+				throw new IOException("Response contains multiple 'Location' headers");
+			}
+
+			return getInvoiceId(locations.get(0));
+
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (JAXBException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	@Override
+	public boolean deleteInvoice(String invoiceId) {
+		if(invoiceId == null || invoiceId.isEmpty()) {
+			throw new IllegalArgumentException("Invalid client trasaction ID");
+		}
+
+		try {
+			final URL url = new URL(String.format(DELETE_INVOICE_STRING, baseUrl, clientSystemId, invoiceId));
+			final HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			connection.setRequestMethod("DELETE");
+			connection.setRequestProperty("Accept", "application/xml,text/xml");
+			connection.setRequestProperty("Authorization", credentialClient.obtainAuthorizationHeaderString());
+			connection.setRequestProperty("Content-Type", "application/xml");
+			connection.getInputStream();
+			System.out.println(connection.getResponseCode());
+			return true;
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+	protected String getInvoiceId(String response) {
+		final String URL = String.format(CREATE_INVOICE_STRING, baseUrl, clientSystemId);
+		final int urlStartIndex = response.indexOf(URL) + URL.length() + 1;
+		final int urlEndIndex = response.indexOf("\n", urlStartIndex);
+		if(urlEndIndex == -1) {
+			return response.substring(urlStartIndex).trim();
+		} else {
+			return response.substring(urlStartIndex, urlEndIndex).trim();
+		}
+	}
+
 	protected File getTempFile(final String personId) {
 		final File tempFile;
 		try {
@@ -124,6 +225,23 @@ public class YpayClientImpl implements YpayClient {
 				return context.createUnmarshaller();
 			} catch (JAXBException e) {
 				throw new IllegalStateException("Unable to create unmarshaller.", e);
+			}
+		}
+	}
+
+	private static class InvoiceMarshallerThreadLocal extends ThreadLocal<Marshaller> {
+		private final JAXBContext context;
+
+		private InvoiceMarshallerThreadLocal(JAXBContext context) {
+			this.context = context;
+		}
+
+		@Override
+		protected Marshaller initialValue() {
+			try {
+				return context.createMarshaller();
+			} catch (JAXBException e) {
+				throw new IllegalStateException("Unable to create marshaller.", e);
 			}
 		}
 	}
