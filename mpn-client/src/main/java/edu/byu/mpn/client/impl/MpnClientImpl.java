64efecd0bef577ebbc40ae9b46d368265075e1b6
Corey Woodfield <corey_woodfield@byu.edu>
2016-05-26 16:48:38 -0600

    Removed dependencies on edu.byu.mpn.domain

--- mpn-client/src/main/java/edu/byu/mpn/client/impl/MpnClientImpl.java
+++ mpn-client/src/main/java/edu/byu/mpn/client/impl/MpnClientImpl.java
@@ -6,8 +6,7 @@ import com.amazonaws.services.sns.AmazonSNSClient;
 import com.amazonaws.services.sns.model.*;
 import com.google.gson.Gson;
 import edu.byu.mpn.client.interfaces.MpnClient;
-import edu.byu.mpn.domain.Device;
-import edu.byu.mpn.helpers.GenericNotification;
+import edu.byu.mpn.client.helpers.GenericNotification;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,16 +28,16 @@ public class MpnClientImpl implements MpnClient {
 		snsClient.setRegion(Region.getRegion(Regions.US_WEST_2));
 	}
 
-	public void pushNotificationToTopic(GenericNotification notification, String topicArn) {
-		publishNotification(notification, topicArn);
+	public PublishResult pushNotificationToTopic(String title, String message, String topicArn) {
+		return publishNotification(new GenericNotification(title, message), topicArn);
 	}
 
-	public boolean pushNotifications(GenericNotification notification, List<String> targetArns) {
+	public boolean pushNotifications(String title, String message, List<String> targetArns) {
 		boolean result = true;
 		for (String targetArn : targetArns) {
 			if (isEndpointEnabled(targetArn)) {
 //				This function throws an exception if the endpoint is disabled, so we added the if instead of doing a try/catch
-				publishNotification(notification, targetArn);
+				publishNotification(new GenericNotification(title, message), targetArn);
 			} else {
 				result = false;
 			}
@@ -46,16 +45,16 @@ public class MpnClientImpl implements MpnClient {
 		return result;
 	}
 
-	public CreatePlatformEndpointResult createPlatformEndpoint(Device device, String platformApplicationArn) throws InvalidParameterException {
-		return snsClient.createPlatformEndpoint(new CreatePlatformEndpointRequest().withToken(device.getToken()).withPlatformApplicationArn(platformApplicationArn));
+	public CreatePlatformEndpointResult createPlatformEndpoint(String token, String platformApplicationArn) throws InvalidParameterException {
+		return snsClient.createPlatformEndpoint(new CreatePlatformEndpointRequest().withToken(token).withPlatformApplicationArn(platformApplicationArn));
 	}
 
-	public void updatePlatformEndpoint(Device device) throws InvalidParameterException {
-		GetEndpointAttributesResult endpointAttributes = snsClient.getEndpointAttributes(new GetEndpointAttributesRequest().withEndpointArn(device.getEndpointArn()));
+	public void updatePlatformEndpoint(String token, String endpointArn) throws InvalidParameterException {
+		GetEndpointAttributesResult endpointAttributes = snsClient.getEndpointAttributes(new GetEndpointAttributesRequest().withEndpointArn(endpointArn));
 		Map<String, String> attributes = endpointAttributes.getAttributes();
-		attributes.put("Token", device.getToken());
+		attributes.put("Token", token);
 		attributes.put("Enabled", "true");
-		snsClient.setEndpointAttributes(new SetEndpointAttributesRequest().withEndpointArn(device.getEndpointArn()).withAttributes(attributes));
+		snsClient.setEndpointAttributes(new SetEndpointAttributesRequest().withEndpointArn(endpointArn).withAttributes(attributes));
 	}
 
 	public boolean isEndpointEnabled(String endpointArn) {
@@ -63,27 +62,24 @@ public class MpnClientImpl implements MpnClient {
 		return "true".equals(enabled);
 	}
 
-	public SubscribeResult subscribeDevice(Device device, String topicArn) {
-		GetEndpointAttributesResult endpointAttributes = snsClient.getEndpointAttributes(new GetEndpointAttributesRequest().withEndpointArn(device.getEndpointArn()));
+	public SubscribeResult subscribeDevice(String endpointArn, String topicArn, String personId, String deviceName) {
+		GetEndpointAttributesResult endpointAttributes = snsClient.getEndpointAttributes(new GetEndpointAttributesRequest().withEndpointArn(endpointArn));
 		Map<String, String> attributes = endpointAttributes.getAttributes();
-		attributes.put("CustomUserData", getUserData(device));
-		snsClient.setEndpointAttributes(new SetEndpointAttributesRequest().withEndpointArn(device.getEndpointArn()).withAttributes(attributes));
-		return snsClient.subscribe(new SubscribeRequest().withTopicArn(topicArn).withEndpoint(device.getEndpointArn()).withProtocol("application"));
+		attributes.put("CustomUserData", String.format("%s - %s", personId, deviceName));
+		snsClient.setEndpointAttributes(new SetEndpointAttributesRequest().withEndpointArn(endpointArn).withAttributes(attributes));
+		return snsClient.subscribe(new SubscribeRequest().withTopicArn(topicArn).withEndpoint(endpointArn).withProtocol("application"));
 	}
 
-	public void unsubscribeDevice(Device device) {
-		GetEndpointAttributesResult endpointAttributes = snsClient.getEndpointAttributes(new GetEndpointAttributesRequest().withEndpointArn(device.getEndpointArn()));
+	public void unsubscribeDevice(String endpointArn, String subscriptionArn) {
+		GetEndpointAttributesResult endpointAttributes = snsClient.getEndpointAttributes(new GetEndpointAttributesRequest().withEndpointArn(endpointArn));
 		Map<String, String> attributes = endpointAttributes.getAttributes();
 		attributes.put("CustomUserData", "");
-		snsClient.setEndpointAttributes(new SetEndpointAttributesRequest().withEndpointArn(device.getEndpointArn()).withAttributes(attributes));
-		snsClient.unsubscribe(new UnsubscribeRequest().withSubscriptionArn(device.getSubscriptionArn()));
+		snsClient.setEndpointAttributes(new SetEndpointAttributesRequest().withEndpointArn(endpointArn).withAttributes(attributes));
+		snsClient.unsubscribe(new UnsubscribeRequest().withSubscriptionArn(subscriptionArn));
 	}
 
 	public PublishResult publishNotification(GenericNotification notification, String targetArn) {
 		return snsClient.publish(new PublishRequest().withMessageStructure("json").withMessage(new Gson().toJson(notification)).withTargetArn(targetArn));
 	}
 
-	private String getUserData(Device device) {
-		return String.format("%s - %s", device.getPersonId(), device.getDeviceName());
-	}
 }
