85796107d4d1f188df958f01205ada5812011002
Scott Hutchings <yoshutch@gmail.com>
2016-11-01 15:55:14 -0600

    httpRestBuilder can do posts

--- core-identity-client/src/main/java/edu/byu/edge/coreIdentity/client/rest/HttpRestBuilder.java
+++ core-identity-client/src/main/java/edu/byu/edge/coreIdentity/client/rest/HttpRestBuilder.java
@@ -2,13 +2,9 @@ package edu.byu.edge.coreIdentity.client.rest;
 
 import edu.byu.edge.coreIdentity.client.exceptions.RestHttpException;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
-import java.net.URL;
+import java.io.*;
+import java.net.*;
+import java.util.Map;
 
 /**
  * Created by Scott Hutchings on 8/30/2016.
@@ -20,6 +16,8 @@ public class HttpRestBuilder {
 	private String accept;
 	private String authorization;
 	private String contentType;
+	private String body;
+	private Map<String, String> queryParams;
 
 	public HttpRestBuilder(String url) {
 		this.url = url;
@@ -40,6 +38,16 @@ public class HttpRestBuilder {
 		return this;
 	}
 
+	public HttpRestBuilder body(String body){
+		this.body = body;
+		return this;
+	}
+
+	public HttpRestBuilder queryParams(Map<String, String> queryParams){
+		this.queryParams = queryParams;
+		return this;
+	}
+
 	public HttpRestBuilder method(String method) {
 		this.method = method;
 		return this;
@@ -47,6 +55,11 @@ public class HttpRestBuilder {
 
 	public String get() throws RestHttpException {
 		this.method = "GET";
+		return execute();
+	}
+
+	public String post() throws RestHttpException {
+		this.method = "POST";
 		return execute();
 	}
 
@@ -65,6 +78,18 @@ public class HttpRestBuilder {
 			if (contentType != null){
 				connection.setRequestProperty("Content-Type", contentType);
 			}
+			if (body != null || queryParams != null){
+				connection.setDoOutput(true);
+				final OutputStream outputStream = connection.getOutputStream();
+				final BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream));
+				if (body != null){
+					bufferedWriter.write(body);
+				} else if (queryParams != null) {
+					bufferedWriter.write(getQueryString(queryParams));
+				}
+				bufferedWriter.flush();
+				bufferedWriter.close();
+			}
 			final int responseCode = connection.getResponseCode();
 			if (responseCode != 200){
 				throw new RestHttpException("Error code: " + connection.getResponseCode() +
@@ -73,7 +98,7 @@ public class HttpRestBuilder {
 			BufferedReader in = new BufferedReader(
 					new InputStreamReader(connection.getInputStream()));
 			String inputLine;
-			StringBuffer response = new StringBuffer();
+			StringBuilder response = new StringBuilder();
 			while((inputLine = in.readLine()) != null) {
 				response.append(inputLine);
 			}
@@ -87,5 +112,21 @@ public class HttpRestBuilder {
 		} catch (IOException e) {
 			throw new RestHttpException("IOException", e);
 		}
+	}
+
+	private String getQueryString(Map<String, String> queryParams) throws UnsupportedEncodingException {
+		StringBuilder result = new StringBuilder();
+		boolean first = true;
+		for (String key : queryParams.keySet()) {
+			if (first) {
+				first = false;
+			} else {
+				result.append("&");
+			}
+			result.append(URLEncoder.encode(key, "UTF-8"));
+			result.append("=");
+			result.append(URLEncoder.encode(queryParams.get(key), "UTF-8"));
+		}
+		return result.toString();
 	}
 }
